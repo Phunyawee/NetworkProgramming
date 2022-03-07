4ef6762daa47259cabb2d161f34263849b7dc17a
Phunyawee <Phunyawee.s@ku.th>
2022-03-07 19:36:34 +0700

    updated ox.py

--- XO/ox.py
+++ XO/ox.py
@@ -13,23 +13,43 @@ root.geometry("1600x800+0+0")
 #root.attributes('-fullscreen', True)  
 root.title("Tic-tac-toe")
 
-Tops = Frame(root,width=1600,height=50,bg="powder blue",relief=SUNKEN)
+Tops = Frame(root,width=1600,bg="#856ff8",relief=SUNKEN)
 Tops.pack(side=TOP)
 
-f1 = Frame(root,width=800,height=700,relief=SUNKEN)
-f1.pack(side=LEFT)
+Bottoms = Frame(root,width=1600,bg="#856ff8",relief=SUNKEN)
+Bottoms.pack(side=BOTTOM)
 
-f2 = Frame(root,width=300,height=700,relief=SUNKEN)
-f2.pack(side=RIGHT)
+f1 = Frame(Bottoms,width=800,height=700,bg="",relief=SUNKEN)
+#f1.pack(side=LEFT)
+f1.grid(row=1,column=1)
+
+f2 = Frame(Bottoms,width=600,height=700,relief=SUNKEN)
+#f2.pack(side=LEFT)
+#f2.pack(side=RIGHT)
+f2.grid(row=1,column=2)
+
+f3 = Frame(Bottoms,width=500,height=700,relief=SUNKEN)
+#f3.pack(side=RIGHT)
+f3.grid(row=1,column=3)
 
 modeNow = StringVar()
+modeMainFrame = StringVar()
+lblInfox = Label(f3,font=('Microsoft YaHei Light',50,'bold'),
+                text="TIC-TAC-TOE",fg="Blue",bd=10,anchor='w')
+lblInfox.grid(row=0,column=0)
+lblRolex = Label(f3,font=('Microsoft YaHei Light',40,'bold'),
+                textvariable=modeMainFrame,fg="Blue",bd=10,anchor='w')
+lblRolex.grid(row=1,column=0)
+
+#f2.pack(side=RIGHT)
+
 
 #label
-lblInfo = Label(Tops,font=('TH Sarabun New',50,'bold'),
-                text="Tic-tac-toe",fg="Blue",bd=10,anchor='w')
+lblInfo = Label(Tops,font=('Microsoft YaHei Light',50,'bold'),
+                text="TIC-TAC-TOE",fg="Blue",bd=10,anchor='w')
 lblInfo.grid(row=0,column=0)
-lblRole = Label(Tops,font=('TH Sarabun New',40,'bold'),
-                textvariable=modeNow,fg="Blue",bd=10,anchor='w')
+lblRole = Label(Tops,font=('Microsoft YaHei Light',40,'bold'),
+                textvariable=modeMainFrame,fg="Blue",bd=10,anchor='w')
 lblRole.grid(row=1,column=0)
 #OX code =======================================================================
 numberToSend=0
@@ -44,6 +64,7 @@ def print_table(msg):
     print (" "+str(msg[6])+" | "+str(msg[7])+" | "+str(msg[8])+" ")
     print ("") 
 def check():
+    print("check")
     global count
     global msg
     global useSlot
@@ -58,11 +79,13 @@ def check():
         if whatRole == 1:
             s.close()
             default("Client")   
-            actionWindow("Reconnect for play again.")
+            tryAgain(True)
+            #actionWindow("Reconnect for play again.")
         if whatRole == 2:
             c.close()   
             default("Server")   
-            actionWindow("Reconnect for play again.")
+            tryAgain(True)
+            #actionWindow("Reconnect for play again.")
                          
     if (msg[3] == msg[4]) and (msg[4] == msg[5]):        
         if (msg[3] == 'x'):
@@ -240,37 +263,46 @@ def clientPlay(tmp):
     elif whatRole == 2:
         count+=1
         print("Count"+str(count))
-        try:
-            tmpS = c.recv(20)
-            putSlot(int(tmpS))
-            print(useSlot)
-            (msg[int(tmpS)-1]) = 'o'
-            number[int(tmpS)-1]=' O '
-            print(number)
-            upDate()
-            onButton()
-        except ValueError:
-            print("Client disconnect")
-            statePlayer.set("Client disconnect")
+        if count== 9:
+               
+            c.close()
             default("Server")
-        except ConnectionAbortedError:
-            print("Client disconnect") 
-            statePlayer.set("Client disconnect")
-            default("Server")
-        except UnboundLocalError:
-            print("Client disconnect")
-            statePlayer.set("Client disconnect")
-            default("Server")
-        except ConnectionResetError :
-            print("Client disconnect")
-            statePlayer.set("Client disconnect")
-            default("Server")
+            offButton()
+            btnSend["state"]= "disabled"
+            count=0
+        else:
+            try:
+                tmpS = c.recv(20)
+                putSlot(int(tmpS))
+                print(useSlot)
+                (msg[int(tmpS)-1]) = 'o'
+                number[int(tmpS)-1]=' O '
+                print(number)
+                upDate()
+                onButton()
+            except ValueError:
+                print("Client disconnect")
+                statePlayer.set("Client disconnect")
+                default("Server")
+            except ConnectionAbortedError:
+                print("Client disconnect") 
+                statePlayer.set("Client disconnect")
+                default("Server")
+            except UnboundLocalError:
+                print("Client disconnect")
+                statePlayer.set("Client disconnect")
+                default("Server")
+            except ConnectionResetError :
+                print("Client disconnect")
+                statePlayer.set("Client disconnect")
+                default("Server")
     print_table(msg)
     check()
     print ("Please Wait !!!")  
 
 
 def putSlot(index):
+    print("putSlot call")
     global useSlot
     if useSlot[index-1] == '0':
         useSlot[index-1] = '1'
@@ -278,15 +310,14 @@ def putSlot(index):
     else:
         return False
 
-
-
-
-
 def whoWin(winner):
+    print("whoWin call")
     if winner == 1:
         statePlayer.set("You win")
+        #tryAgain()
     else:
         statePlayer.set("You lose")
+        #tryAgain()
 
 def serverPlay(tmp):
     print("serverPlay")
@@ -300,8 +331,7 @@ def serverPlay(tmp):
             s.close()
             default("Client")
             count=0
-        else:
-            
+        else:    
             try:
                 tmp = s.recv(20)
                 putSlot(int(tmp))
@@ -359,10 +389,6 @@ def serverPlay(tmp):
         
     print_table(msg)
     check()
-    
-
-
-
 
 #OX code =======================================================================
 #Variable
@@ -384,16 +410,20 @@ changeModeLabel = StringVar()
 
 
 def configMode():  
+    print("configMode call")
     if configAllow == True:
-        if(btnConfig['text']=='Server'):
-            btnConfig['text']='Client'
-            modeNow.set('Client')
-            default('Client')
-            #default()
-        else:
-            btnConfig['text']='Server'
+        if whatRole==1:
+            
             modeNow.set('Server')
             default('Server')
+            btnConfig['text']='Server'
+    
+            #default()
+        elif whatRole==2:
+            
+            modeNow.set('Client')
+            default('Client')
+            btnConfig['text']='Client'
             #default()
     else:
         print("Not allow")
@@ -401,26 +431,34 @@ def configMode():
     
 configAllow = True
 def configuration():
-    
+    print("configuration call")
     global btnConfig
     def close():
         changeModeLabel.set("")
         made.destroy()
-    made = Tk()
+    made = Toplevel(root)
     made.geometry('300x300')
     made.title("Config")
-    txtLabel=Label(made,font=('arial',13,'bold'),text="Mode")
+    txtLabel=Label(made,font=('Microsoft YaHei Light',13,'bold'),text="Mode")
     txtLabel.pack()
-    btnConfig=Button(made,font=('arial',11,'bold'),text='Client',command=configMode)
-    btnConfig.pack()
+    if whatRole == 1:
+
+        btnConfig=Button(made,font=('Microsoft YaHei Light',11,'bold'),text='Server',command=configMode)
+        btnConfig.pack()
+    elif whatRole == 2:
+        btnConfig=Button(made,font=('Microsoft YaHei Light',11,'bold'),text='Client',command=configMode)
+        btnConfig.pack()
     
-    btnClose=Button(made,font=('arial',11,'bold'),text='close',command=close).pack(side=BOTTOM)
+    btnClose=Button(made,font=('Microsoft YaHei Light',11,'bold'),text='close',command=close).pack(side=BOTTOM)
     made.mainloop()
 allowServerSend = False
 #Default
 def default(role):
-
+    print("default call")
+    #global btnConfig
     #global role
+    # 1 = client
+    # 2 = server
     global whatRole,count
     count=0
     #global client
@@ -428,14 +466,20 @@ def default(role):
     #global server
     global serversocket,c,addr,allowServerSend
     if role == "Client":
+        #btnConfig['text']='Server'
         whatRole = 1
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        modeNow.set('Client')
+        modeMainFrame.set('Client')
+        modeNow.set('Server')
+        
     elif role =="Server":
+        #btnConfig['text']='Client'
         allowServerSend = False
         whatRole = 2
         serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        modeNow.set('Server')
+        modeMainFrame.set('Server')
+        modeNow.set('Client')
+        
     text1.set('    ')
     text2.set('    ')
     text3.set('    ')
@@ -567,9 +611,12 @@ def btnClick(numbers):
                     offButton()
                     btnSend["state"]= "normal"
     elif whatRole == 2:
-        if count== 10:
+        if count== 9:
+           
             c.close()
             default("Server")
+            offButton()
+            btnSend["state"]= "disabled"
             count=0
         else:
             if stateBtn[numbers-1]==False:
@@ -585,8 +632,34 @@ def btnClick(numbers):
                     offButton()
                     btnSend["state"]= "normal"
                     allowServerSend = True
-                
-                
+
+def closeGame():
+    End = tkinter.messagebox.askyesno("TIC TAC TOE","Confirm exit")
+    if End > 0:
+        root.destroy()
+        return              
+def tryAgain():
+    top = Toplevel(root) 
+    top.geometry("500x230")
+    frame = Frame(top)
+    top.resizable(False, False)
+    try_lb = Label(top,padx=16,pady=32,bd=8,fg="black",
+    font=font1, text="Try Again or Leave a Game")
+    try_btn = Button(frame,padx=16,pady=16,bd=8,fg="black",
+              font=font1,
+              text="Try again",width=10,
+              height=1,bg="powder blue")
+    leave_btn = Button(frame,padx=16,pady=16,bd=8,fg="black",
+              font=font1,
+              text="Leave a game",width=15,
+              height=1,bg="powder blue")
+              
+    try_lb.pack()       
+    frame.pack()
+    try_btn.grid(row=1,column=1)
+    leave_btn.grid(row=1,column=2)
+    top.mainloop()         
+
 
 def actionWindow(whatAction):
     top = Toplevel(root) 
@@ -687,7 +760,7 @@ def getStart():
     #print(port)
 #==========================Fonts==========================================
 #font.Font(family='Helvetica', size=12, weight='bold')
-font1 = TkFont.Font(family="Consolas",size = 20,weight = 'bold')
+font1 = TkFont.Font(family="Microsoft YaHei Light",size = 20,weight = 'bold')
 
 #===============================Calculator Frame===========================
 txtDisplayState = Label(f2,font=font1,
@@ -697,7 +770,7 @@ txtDisplayState = Label(f2,font=font1,
 txtDisplayState.grid(columnspan=4)
 txtDisplay = Entry(f2,font=font1,
                    textvariable=statePlayer,bd=30,insertwidth=4,
-                   bg="powder blue",justify='right',state='disabled',disabledbackground='powder blue')
+                   bg="#49A",justify='right',state='disabled',disabledbackground='powder blue')
 txtDisplay.grid(columnspan=4)
 
 btnWidth = 5
@@ -708,37 +781,37 @@ btn1 = Button(f2,padx=16,pady=16,bd=8,fg="black",
               font=font1,
               textvariable=text1,width=btnWidth,
               height=btnHeight,
-              bg="powder blue",command=lambda:btnClick(1))
+              bg="#49A",command=lambda:btnClick(1))
 btn1.grid(row=3,column=0)
 
 btn2 = Button(f2,padx=16,pady=16,bd=8,fg="black",
               font=font1,
               textvariable=text2,width=btnWidth,
-              height=btnHeight,bg="powder blue",command=lambda:btnClick(2))
+              height=btnHeight,bg="#49A",command=lambda:btnClick(2))
 btn2.grid(row=3,column=1)
 
 btn3 = Button(f2,padx=16,pady=16,bd=8,fg="black",
               font=font1,
               textvariable=text3,width=btnWidth,
-              height=btnHeight,bg="powder blue",command=lambda:btnClick(3))
+              height=btnHeight,bg="#49A",command=lambda:btnClick(3))
 btn3.grid(row=3,column=2)
 #===============================Row4=======================================
 btn4 = Button(f2,padx=16,pady=16,bd=8,fg="black",
               font=font1,
               textvariable=text4,width=btnWidth,
-              height=btnHeight,bg="powder blue",command=lambda:btnClick(4))
+              height=btnHeight,bg="#49A",command=lambda:btnClick(4))
 btn4.grid(row=4,column=0)
 
 btn5 = Button(f2,padx=16,pady=16,bd=8,fg="black",
               font=font1,
               textvariable=text5,width=btnWidth,
-              height=btnHeight,bg="powder blue",command=lambda:btnClick(5))
+              height=btnHeight,bg="#49A",command=lambda:btnClick(5))
 btn5.grid(row=4,column=1)
 
 btn6 = Button(f2,padx=16,pady=16,bd=8,fg="black",
               font=font1,
               textvariable=text6,width=btnWidth,
-              height=btnHeight,bg="powder blue",command=lambda:btnClick(6))
+              height=btnHeight,bg="#49A",command=lambda:btnClick(6))
 btn6.grid(row=4,column=2)
 
 #===============================Row3=======================================
@@ -746,58 +819,62 @@ btn6.grid(row=4,column=2)
 btn7 = Button(f2,padx=16,pady=16,bd=8,fg="black",
               font=font1,
               textvariable=text7,width=btnWidth,
-              height=btnHeight,bg="powder blue",command=lambda:btnClick(7))
+              height=btnHeight,bg="#49A",command=lambda:btnClick(7))
 btn7.grid(row=5,column=0)
 
 btn8 = Button(f2,padx=16,pady=16,bd=8,fg="black",
               font=font1,
               textvariable=text8,width=btnWidth,
-              height=btnHeight,bg="powder blue",command=lambda:btnClick(8))
+              height=btnHeight,bg="#49A",command=lambda:btnClick(8))
 btn8.grid(row=5,column=1)
 
 btn9 = Button(f2,padx=16,pady=16,bd=8,fg="black",
               font=font1,
               textvariable=text9,width=btnWidth,
-              height=btnHeight,bg="powder blue",command=lambda:btnClick(9))
+              height=btnHeight,bg="#49A",command=lambda:btnClick(9))
 btn9.grid(row=5,column=2)
 
 btnSend = Button(f2,padx=16,pady=16,bd=8,fg="black",
               font=font1,
-              text="send",bg="powder blue",command=communication)
-btnSend.grid(row=6,column=2)
+              text="SEND",width=23,
+              height=btnHeight,bg="#49A",command=communication)
+btnSend.grid(row=6,columnspan=3)
 
 offButton()
 
 lblReference = Label(f1,font=font1, text="State",
                      bd=16,anchor='w').grid(row=0,column=0)  
 txtReference = Entry(f1,font=font1,textvariable=stateServer,
-                     bd=10,insertwidth=4,bg='powder blue',justify='right',state='disabled',disabledbackground='powder blue').grid(row=0,column=1)
+                     bd=10,insertwidth=4,bg='#49A',justify='right',state='disabled',disabledbackground='powder blue').grid(row=0,column=1)
 
 #-----------------------------------------------------------------------------------------------------
 lblIp = Label(f1,font=font1, text="ip"
                  ,bd=16,anchor='w').grid(row=1,column=0)  
 txtIp = Entry(f1,font=font1,textvariable=ip_Input,
-                     bd=10,insertwidth=4,bg='powder blue',justify='right')
+                     bd=10,insertwidth=4,bg='white',justify='right')
 txtIp.grid(row=1,column=1)
 
 #-----------------------------------------------------------------------------------------------------
 lblPort = Label(f1,font=font1, text="port"
                  ,bd=16,anchor='w').grid(row=2,column=0)  
 txtPort = Entry(f1,font=font1,textvariable=port_Input,
-                     bd=10,insertwidth=4,bg='powder blue',justify='right')
+                     bd=10,insertwidth=4,bg='white',justify='right')
 txtPort.grid(row=2,column=1)  
 
 btnStart = Button(f1,padx=16,pady=16,bd=16,fg="black",font=font1,
-                  width=10,text="Connect",bg="powder blue",command=getStart)
+                  width=10,text="Connect",bg="#49A",command=getStart)
 btnStart.grid(row=7,column=1)
+btnTry = Button(f1,padx=16,pady=16,bd=16,fg="black",font=font1,
+                  width=5,text="Exit",bg="#49A",command=closeGame)
+btnTry.grid(row=8,column=1)
 menubar= Menu(root)
 filemenu = Menu(menubar, tearoff =0)
 menubar.add_cascade(label="File",menu=filemenu)
 filemenu.add_command(label="Config",command=configuration)
 filemenu.add_command(label="End",command=disConnected)
-
+filemenu.add_command(label="Exit", command = closeGame)
 root.config(menu=menubar)
-
-
+root.attributes('-fullscreen', True)  
+root.resizable(False,False)
 default("Client")
 root.mainloop()  
